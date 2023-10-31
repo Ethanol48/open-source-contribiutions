724f366b5d6c7c4cbeb693407e3b8c0f971dda41
Ethan Rouimi <contact@ethan-rouimi.com>
2023-10-31 19:39:53 +0100

    print events

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const fs = require("fs");
+const path = require("path");
+const { spawn } = require("child_process");
+const { Toolkit } = require("actions-toolkit");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
 
-  console.log("This is the index file")
+Toolkit.run(async (tools) => {
+  tools.log.debug(`Getting activity for ${GH_USERNAME}`);
+  const events = await tools.github.activity.listPublicEventsForUser({
+    username: GH_USERNAME,
+    per_page: 100,
+  });
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+  tools.log.debug(
+    `Activity for ${GH_USERNAME}, ${events.data.length} events found.`
+  );
+
+  console.log(events[0]);
+  console.log(events[100]);
+
+
+
+});
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+
+//   console.log("This is the index file")
+
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
 
